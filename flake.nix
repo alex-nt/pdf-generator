50d725cfa9696a1eeb8302b4fdf8b9f7b90eb593
Alex <12470950+alex-nt@users.noreply.github.com>
2024-06-01 09:48:34 +0300

    Update to latest

--- flake.nix
+++ flake.nix
@@ -1,32 +1,29 @@
 {
   description = "A simple Go package";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
 
-  # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/master";
-
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-
-      # to work with older version of flakes
       lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
 
       # Generate a user-friendly version number.
       version = builtins.substring 0 8 lastModifiedDate;
 
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-
     in
     {
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixfmt-rfc-style);
 
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
+      packages = forAllSystems (
+        system:
         let
           pkgs = nixpkgsFor.${system};
         in
@@ -48,24 +45,28 @@
             # remeber to bump this hash when your dependencies change.
             #vendorSha256 = pkgs.lib.fakeSha256;
 
-            vendorHash = "sha256-65fV3NLSjKPYDh4lgRQTi4IBdfcBh2c1rq0aXy78aA0=";
+            vendorHash = "sha256-nYGDOVd1y/NLVOHZYzbxPN/opCMqj6ARhhalLi5BZbs=";
           };
-        });
-      
-      # Add dependencies that are only needed for development
-      devShells = forAllSystems (system:
-        let 
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
           pkgs = nixpkgsFor.${system};
         in
         {
           default = pkgs.mkShell {
-            buildInputs = with pkgs; [ go gopls gotools go-tools ];
+            buildInputs = with pkgs; [
+              go
+              gopls
+              gotools
+              go-tools
+            ];
           };
-        });
+        }
+      );
 
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
       defaultPackage = forAllSystems (system: self.packages.${system}.pdf-generator);
     };
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/alex-nt/pdf-generator
 
-go 1.21
+go 1.22
 
 require (
 	github.com/jung-kurt/gofpdf/v2 v2.17.3
-	golang.org/x/image v0.15.0
+	golang.org/x/image v0.16.0
 )
